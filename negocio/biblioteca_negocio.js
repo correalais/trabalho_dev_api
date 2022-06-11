825986f1ea75469b3d9505dafa1079f669710403
Lais Correa <laiscorreamartins@gmail.com>
2022-06-11 16:51:43 -0300

    criação das funcoes de cadastro

---
+++ negocio/biblioteca_negocio.js
@@ -0,0 +1,35 @@
+const bibliotecaRepository = require('../biblioteca_persistencia')
+
+ function cadastrarLivro (livro, callback) {
+    if(!livro || !livro.isbn || !livro.nome || !livro.autor || !livro.editora || !livro.ano){
+        const erro = "Preencha todos os campos!";
+        callback(erro, undefined)
+    }
+    else {
+        bibliotecaRepository.cadastrarLivro(livro, callback);
+    }  
+} 
+
+function cadastrarAutor (autor, callback) {
+    if(!autor || !autor.id||  !autor.nome || !autor.pais){
+        const erro = "Preencha todos os campos!";
+        callback(erro, undefined)
+    }
+    else {
+        bibliotecaRepository.cadastrarAutor(autor, callback);
+    }  
+}
+
+function cadastrarCliente (cliente, callback) {
+    if(!cliente|| !cliente.matricula||  !cliente.nome || !cliente.telefone){
+        const erro = "Preencha todos os campos!";
+        callback(erro, undefined)
+    }
+    else {
+        bibliotecaRepository.cadastrarCliente(cliente, callback);
+    }  
+} 
+
+module.exports = {
+    cadastrarLivro, cadastrarAutor, cadastrarCliente
+} 
